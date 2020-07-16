003cfb46bc7c9719dccad91b8e72cf26d59b4d49
paulopimenta6 <paulopimenta6@gmail.com>
2020-07-16 02:46:53 -0300

    adicionado codigo em C ainda por fazer usando repeticoes

---
+++ c_cpp/algoritmo_da_amizade.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	
+	char casa, refeicao, bebida, compartilho;
+	char interesse[30];
+	int tipo;
+	
+	do{
+		printf("Alguem em casa? \n");
+		scanf("%c", &casa);
+		fflush(stdin);
+		
+		if(casa=='s'){
+			Break;
+		}
+		else{
+			printf() 
