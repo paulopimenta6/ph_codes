de21976ae5b260afc9206bc4326778a307346611
paulopimenta6 <paulopimenta6@gmail.com>
2020-07-17 04:16:13 -0300

    Algoritmo da amizade feito e colocado no github por novossibirsk

--- c_cpp/algoritmo_da_amizade.c
+++ c_cpp/algoritmo_da_amizade.c
@@ -13,7 +13,76 @@ int main(){
 		fflush(stdin);
 		
 		if(casa=='s'){
-			Break;
+			break;
 		}
 		else{
-			printf() 
+			printf("Deixe mensagem \n");
+			printf("Aguarde pelo retorno \n");
+		}
+	}while(casa!='s');
+	
+	printf("Gostaria de compartilhar uma refeição? s/n \n");
+	scanf("%c", &refeicao);
+	
+	if(refeicao=='s'){
+		printf("Vamos jantar! \n");
+		printf("inicio da amizade!!! \n");
+	}
+	else{
+		printf("Gostaria de uma bebida quente? s/n \n");
+		fflush(stdin);
+		scanf("%c", &bebida);
+		
+		if(bebida=='s'){
+			printf("1 - cha \n");
+			printf("2 - cafe \n");
+			printf("3 - chocolate \n");
+			
+			fflush(stdin);
+			tipo=getchar();
+			
+			switch(tipo){
+				case '1':
+				    printf("cha \n");
+				    break;
+				case '2':
+				    printf("cafe \n");
+				    break;
+				case '3':
+				    printf("chocolate \n");
+				    break;        
+				default:
+				    printf("Opção não válida! \n");
+				    break;
+				}
+				
+			}
+			
+		else{
+			printf("Conte-me dos seus interesses! \n");
+			scanf("%s", interesse);
+			printf("O interesse é: %s \n", interesse);
+			
+			printf("Compartilho deste interesse? \n");
+			fflush(stdin);
+			scanf("%c", &compartilho);
+			
+			if(compartilho=='s'){
+				printf("Por que não fazemos aquilo juntos? \n");
+			}
+			else{
+				printf("Não compartilho deste interesse... \n");
+			}
+		}
+		
+		printf("Inicio da amizade \n");
+		
+	}
+	
+	return 0;
+		
+}
+		
+			
+			
+	 
