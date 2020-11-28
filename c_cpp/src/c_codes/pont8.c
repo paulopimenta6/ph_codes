0ab700b28f0679dcf6c7ad140033c1c125a1d113
paulopimenta6 <paulopimenta315@gmail.com>
2020-11-28 09:43:13 -0300

    algoritmo de alocacao de memoria dinamica

--- c_cpp/src/c_codes/pont8.c
+++ c_cpp/src/c_codes/pont8.c
@@ -1,1 +1,23 @@
+//alocacao de memoria dinamicamente
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+	
+	int *p;
+	p=(int*)malloc(sizeof(int));	
+	
+	if(!p){
+		printf("Memoria insuficiente \n");
+		exit(1);
+	}
+	else{
+		printf("Memoria alocada com sucesso! \n");
+	}
+	
+	free(p);
+	
+	return 0;
+	
+}
+	
