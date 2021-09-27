06b86e70d0bd65044143e6db5e5273a14932089b
Paulo Pimenta <paulopimenta6@gmail.com>
2021-09-27 00:25:24 -0300

    remocao dos arquivos MET

--- python/pythonGrib/bash/testif.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if test -n "${1}" && [[ "${1}" == "teste" ]]
-then
-    echo "Nao vazio"
-    echo "${1}"
-else
-    echo "Vazio"
-fi
--- python/pythonGrib/prototitpo_GEN/cria_mapas_MET_2GEN.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  4 15:29:44 2021
-
-@author: phpimenta
-"""
-
