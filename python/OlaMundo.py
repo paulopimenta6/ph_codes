38b548bd539f7bf76e29511fb51340be3484ab4e
Paulo Pimenta <paulopimenta6@gmail.com>
2023-05-18 01:56:32 -0300

    ajustes realizados

---
+++ python/OlaMundo.py
@@ -0,0 +1,27 @@
+#print("Estudo de Python")
+
+#a="ABCDEF"
+#print(a)
+
+#print(a[1])
+#print(a[2])
+
+#b = "GHIJKLM"
+
+#c = a+b
+
+#print(c)
+
+#print(a*10)
+
+#num = 22
+#num1 = num*3.14
+#print("%.1f" %num1)
+
+#valor = int(input("Digite um valor: "))
+#print(valor)
+
+#dado1 = int(input("Digite um inteiro: "))
+#dado2 = float(input("Digite um valor float: "))
+#dado3 = dado1*dado2
+#print(dado1*dado2)
---
+++ python/listasNovo1.py
@@ -0,0 +1,8 @@
+lista1 = range(100)
+lista2=range(101,201)
+novo=[]
+
+print(len(lista1) == len(lista2))
+
+for i in lista1:
+    print(i)
