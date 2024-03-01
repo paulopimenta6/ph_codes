b4d4c76a75a0779b4e07de3f969b6456bd19123a
paulopimenta6 <paulopimenta6@gmail.com>
2024-03-01 01:09:59 -0300

    R

--- R/regasDeAssociacao/regrasAssociacao01/README.md
+++ R/regasDeAssociacao/regrasAssociacao01/README.md
@@ -1,3 +1,6 @@
+![Ubuntu](https://img.shields.io/badge/Ubuntu-Linux-orange) ![R](https://img.shields.io/badge/R-276DC3?logo=r&logoColor=white&style=flat)
+![RStudio](https://img.shields.io/badge/RStudio-75AADB?logo=rstudio&logoColor=white&style=flat)
+
 # Association Rules in R
 
 It is an example of code for assocation rules in R. It will be used libraries `arules` e `arulesViz`.
@@ -19,3 +22,28 @@ Do not forget to install Linux library libglpk-dev by command:
 ```bash
 sudo apt-get install libglpk-dev
 ```
+
+## Datas
+
+Data used is just an example of binary numbers in a false scenario of a large NATO aerial operation including the main aicraft of alliance members 
+
+## First results
+
+|    LHS   |   RHS    | Support  |Confidence|   Lift   |  Count   |
+|----------|----------|----------|----------|----------|----------|
+|  EF2000  |  Result  |   0.3    |   1.00   |   1.66   |    3     |
+|  Gripen  |  Result  |   0.5    |   0.71   |   1.90   |    5     |
+|   F35    |  Result  |   0.4    |   0.50   |   0.83   |    4     |
+|   F22    |  Result  |   0.5    |   0.55   |   0.92   |    5     |
+|   F15    |  Result  |   0.5    |   0.55   |   0.92   |    5     |
+|  Rafale  |  Result  |   0.6    |   0.60   |   1.00   |    6     |
+
+
+|    LHS   |   RHS    | Support  |Confidence|   Lift   |  Count   |
+|----------|----------|----------|----------|----------|----------|
+|  Result  |  EF2000  |    0.3   |   0.50   |   1.66   |    3     |
+|  Result  |  Gripen  |    0.5   |   0.83   |   1.19   |    5     |
+|  Result  |    F35   |    0.4   |   0.66   |   0.83   |    4     |
+|  Result  |    F22   |    0.5   |   0.83   |   0.92   |    5     |
+|  Result  |    F15   |    0.5   |   0.83   |   0.92   |    5     |
+|  Result  |  Rafale  |    0.6   |   1.00   |   1.00   |    6     |
--- R/regasDeAssociacao/regrasAssociacao01/algoritmoAprioriJogoDeGuerra.R
+++ R/regasDeAssociacao/regrasAssociacao01/algoritmoAprioriJogoDeGuerra.R
@@ -1,19 +1,38 @@
 library(arules)
 library(arulesViz)
+
+library(ggplot2)
+library(plotly)
+
 ###Avioes usados em jogo de guerra
-f16 <- c(1, 1, 0, 1, 0, 1, 0, 1, 0, 0)
-f15 <- c(1, 1, 0, 1, 1, 1, 1, 1, 1, 1)
-f18 <- c(1, 0, 1, 0, 1, 0, 0, 1, 0, 1)
-ef2 <- c(0, 1, 0, 1, 0, 0, 1, 0, 0, 0)
-rfl <- c(0, 0, 1, 1, 1, 1, 0, 0, 1, 1)
-mir <- c(1, 0, 0, 0, 1, 1, 1, 1, 0, 0)
-tor <- c(1, 1, 1, 0, 1, 0, 1, 1, 1, 0)
-grp <- c(0, 0, 1, 1, 0, 1, 1, 0, 1, 1) #where 1 is presence and 0 absent 
+F35 <- c(1, 1, 1, 1, 1, 1, 0, 1, 0, 1)
+F22 <- c(1, 1, 1, 1, 1, 1, 0, 1, 1, 1)
+F18 <- c(1, 0, 1, 0, 1, 0, 0, 1, 0, 1)
+F16 <- c(1, 1, 0, 1, 0, 1, 0, 1, 0, 0)
+F15 <- c(1, 1, 0, 1, 1, 1, 1, 1, 1, 1)
+EF2000 <- c(0, 1, 0, 1, 0, 0, 1, 0, 0, 0)
+Gripen <- c(1, 0, 1, 1, 0, 1, 1, 0, 1, 1)
+Mirage2000_5 <- c(1, 0, 0, 0, 1, 1, 1, 1, 0, 0)
+Panavia_Tornado <- c(1, 0, 0, 0, 1, 0, 1, 0, 1, 0)
+Rafale <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)  #where 1 is presence and 0 absent
 
 #campaign results of all exercises involving all NATO forces
-res <- c(0, 1, 1, 1, 0, 0, 1, 0, 1, 1) #where 1 e victory e 0 e fail
+result <- c(0, 1, 1, 1, 0, 0, 1, 0, 1, 1) #where 1 e victory e 0 e fail
 
-resAndfighters <- cbind(f18, f16, f15, ef2, rfl, mir, tor, grp, res)
-
-
-
+resAndfighters <- cbind(F35, F22, F18, F16, F15, EF2000, Gripen, Mirage2000_5, Panavia_Tornado, Rafale, result)
+print(resAndfighters)
+###############################################################################################
+rules <- apriori(resAndfighters, parameter = list(support = 0.3, confidence = 0.5, target = "rules"))
+rulesV1 <- rules[10:length(rules)]
+###############################################################################################
+inspect(rulesV1, ruleSep = "-->", itemSep = " && ", setStart = "", setEnd = "", linebreak = FALSE)
+###############################################################################################
+plot(rulesV1, main = NULL) 
+###############################################################################################
+rules_subset <- subset(rulesV1, (rhs %in% "result"))
+inspect(rules_subset[1:6])
+###############################################################################################
+plot(rules_subset, method="graph", main = NULL)
+###############################################################################################
+rules_subset <- subset(rulesV1, (lhs %in% "result"))
+inspect(rules_subset[1:6])
