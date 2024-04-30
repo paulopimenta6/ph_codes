40cb070da00c158b4535314b4c916d549f26475f
paulopimenta6 <paulopimenta6@gmail.com>
2024-04-30 04:28:09 -0300

    Ajuste dos codigos de R para analises do elsa

--- R/elsa/src/fazUsoCetoprufeno.R
+++ R/elsa/src/fazUsoCetoprufeno.R
@@ -1,7 +1,7 @@
 ############################################################################################
 ##############################Especificando diretorio src###################################
 ############################################################################################
-source("script_analise_dados_elsa_Var_Lib.R") 
+source("./src/script_analise_dados_elsa_Var_Lib.R") 
 library(scales) # to access break formatting functions
 library(tidyverse)
 library(dplyr)
--- R/elsa/src/habito_fumar_elsa.R
+++ R/elsa/src/habito_fumar_elsa.R
@@ -1,10 +1,7 @@
 ############################################################################################
 ##############################Especificando diretorio src###################################
-if (getwd() != "C:/Users/Paulo_Pimenta/Documents/meus_codigos/ph_codes/R/elsa/src") {   ##
-  setwd("C:/Users/Paulo_Pimenta/Documents/meus_codigos/ph_codes/R/elsa/src")            ##  
-}                                                                                         ##
 ############################################################################################
-source("script_analise_dados_elsa_Var_Lib.R") 
+source("./src/script_analise_dados_elsa_Var_Lib.R") 
 library(scales) # to access break formatting functions
 library(tidyverse)
 library(dplyr)
