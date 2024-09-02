e9ae3f2e50913c81130564f6a4cba2e419fccea0
Paulo Henrique <paulopimenta6@gmail.com>
2024-09-02 02:44:15 -0300

    ajuste do modelo de regressão logistica

--- R/elsa/src/regLogisticaBinariaV3.R
+++ R/elsa/src/regLogisticaBinariaV3.R
@@ -1,30 +1,33 @@
 if(!require(pacman)) install.packages("pacman")
 library(pacman)
-pacman::p_load(dplyr, psych, car, MASS, DescTools, QuantPsyc, ggplot2, VIM, fpp)
+pacman::p_load(dplyr, psych, car, MASS, DescTools, QuantPsyc, ggplot2, VIM, fpp, car)
 source("./src/script_analise_dados_elsa_Var_Lib.R")
 source("./src/dadosRegLogistica.R")
 
-################################################################################
-#coluna <- dataGLM$sod_onda1
-#Q1 <- quantile(coluna, 0.25)
-#Q3 <- quantile(coluna, 0.75)
-#IQR <- Q3 - Q1
-#limite_inferior <- Q1 - 1.5 * IQR
-#limite_superior <- Q3 + 1.5 * IQR
-#outliers <- which(coluna < limite_inferior | coluna > limite_superior)
-#dataGLM_sem_outliers <- dataGLM[-outliers,]
-################################################################################
-model2 <- glm(hip_onda1 ~ sod_onda1, 
-              family = binomial(link="logit"), data = dataGLM, weights = ifelse(dataGLM$hip_onda1 == "S", 2, 1))
-summary(stdres(model2))
-################################################################################
-dataGLM$predicted_hip_onda1 <- predict(model2, type="response")
-plot(predicted_hip_onda1 ~ sod_onda1, data = dataGLM)
+dadosOnda1 <- data.frame(hip = dataGLM$hip_onda1,
+                         pot = dataGLM$pot_onda1,
+                         sod = dataGLM$sod_onda1,
+                         albCreat = dataGLM$albCreat_onda1,
+                         pas = dataGLM$PAS_onda1,
+                         pad = dataGLM$PAD_onda1,
+                         filt = dataGLM$filt_onda1
+)
 
-# Criar um gráfico para visualizar o modelo ajustado (Transmissão vs Peso)
-ggplot(dataGLM, aes(x = sod_onda1, y = predicted_hip_onda1)) +
-  geom_point() +
-  geom_smooth(method = "lm", method.args = list(family = binomial), se = FALSE, color = "red") +
-  labs(title = "Modelo de Regressão Logística: HAS vs Sodio",
-       x = "Concentração de sódio",
-       y = "Probabilidade de Presença de hipertensão Sistêmica")
+###Transformacao de hip em variavel dummy para usar proporcao
+dadosOnda1$hip <- ifelse(dadosOnda1$hip == "S", 1, 0)
+dadosOnda1$ntotal <- 1
+################################################################################
+head(dadosOnda1)
+str(dadosOnda1)
+summary(dadosOnda1)
+################################################################################
+m1 <- glm(data = dadosOnda1, (hip/ntotal)~pot*sod*albCreat*pas*pad*filt, family = binomial, weights = ntotal, control = glm.control(maxit = 50))
+m1_simples <- glm(data = dadosOnda1, (hip/ntotal) ~ pot + sod + albCreat + pas + pad + filt, family = binomial, weights = ntotal)
+m1nulo <- glm((hip/ntotal)~1, family = binomial, weights = ntotal)
+
+anova(m1nulo, m1, test="Chisq")
+anova(m1, test="Chisq")
+vif(m1)
+anova(m1nulo, m1_simples, test="Chisq")
+vif(m1_simples)
+anova(m1_simples, test="Chisq")
