a21f25f2ae8093f880a3c521ade02e8a78296612
Paulo Henrique <paulopimenta6@gmail.com>
2024-11-12 00:26:01 -0300

    atualizacao dos meus estudos de R e Java

--- R/elsa/src/regLogisticaBinariaV5.R
+++ R/elsa/src/regLogisticaBinariaV5.R
@@ -1,7 +1,7 @@
 if(!require(pacman)) install.packages("pacman")
 library(pacman)
 pacman::p_load(dplyr, psych, car, MASS, DescTools, QuantPsyc, ggplot2, survey)
-source("./src/script_analise_dados_elsa_Var_Lib.R")
+#source("./src/script_analise_dados_elsa_Var_Lib.R")
 source("./src/dadosRegLogistica.R")
 
 dadosOnda1 <- data.frame(hip = dataGLM$hip_onda1,
@@ -12,12 +12,6 @@ dadosOnda1 <- data.frame(hip = dataGLM$hip_onda1,
 View(dadosOnda1)
 glimpse(dadosOnda1)
 ################################################################################
-###Ocorre um desbalancemaento das classes
-###S    N 
-###1652 3409
-###Isso significa que há quase 2x mais "N" em relacao a "S"
-################################################################################
-dadosOnda1$hip <- relevel(dadosOnda1$hip, ref = "S")
 table(dadosOnda1$hip)
 summary(dadosOnda1$hip)
 ################################################################################
@@ -26,8 +20,10 @@ peso_classes <- ifelse(dadosOnda1$hip == "S", freq["N"] / freq["S"], 1)
 ################################################################################
 mod <- glm(hip ~ pot + sod, 
            data = dadosOnda1, 
-           family = binomial(link = 'logit'), 
-           weights = peso_classes)
+           family = binomial(link = 'logit')
+)
+################################################################################
+summary(mod)
 ################################################################################
 plot(mod, which = 5)
 summary(stdres(mod))
@@ -47,7 +43,38 @@ dadosOnda1$intolog_sod <- intolog_sod
 ################################################################################
 modint <- glm(hip ~ pot + sod + dadosOnda1$intolog_pot + dadosOnda1$intolog_sod, 
               data = dadosOnda1, 
-              family = binomial(link = 'logit'), 
-              weights = peso_classes)
+              family = binomial(link = 'logit')
+)
 ################################################################################
 summary(modint)
+################################################################################
+logito <- modint$linear.predictors
+################################################################################
+#### Analise da relaca linear
+
+ggplot(dadosOnda1, aes(logito, sod)) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") +
+  theme_classic()
+
+ggplot(dadosOnda1, aes(logito, pot)) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") +
+  theme_classic()
+################################################################################
+### Considerando o modelo original inicial
+dadosOnda1$prob_predita_mod <- predict(mod, type = "response")
+
+# Visualizando as probabilidades em relação a 'pot'
+ggplot(dadosOnda1, aes(x = pot, y = prob_predita_mod)) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") +
+  labs(x = "Potássio (pot)", y = "Probabilidade prevista de Hipertensao") +
+  theme_classic()
+
+# Visualizando as probabilidades em relação a 'sod'
+ggplot(dadosOnda1, aes(x = sod, y = prob_predita_mod)) +
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") +
+  labs(x = "Sódio (sod)", y = "Probabilidade prevista de Hipertensao") +
+  theme_classic()
