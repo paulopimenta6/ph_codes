fc593dc9b10542155cf5df6e9137cb31c40753b3
Paulo Henrique <paulopimenta6@gmail.com>
2025-01-12 03:49:15 -0300

    Estudos de sql baseados em sqlite3

---
+++ db/db_manga/insert_tabela_destino_exportacao.sql
@@ -0,0 +1,23 @@
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(12, 'Reino de Minami', 100, 'Mar do sul');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(15, 'Reino de Paronu', 200, 'Central');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(22, 'Reino de Tokanta', 160, 'Mar do norte');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(23, 'Reino de Alfa', 120, 'Mar do norte');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(25, 'Reino de Ritol', 150, 'Mar do sul');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(30, 'Reino de Sazanna', 80, 'Mar do sul');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(31, 'Reino de Taharu', 240, 'Mar do norte');
+
+INSERT INTO destinos_exportacao(codigo_destino_de_exportacao, nome_de_destino_de_exportacao, populacao, distritos)
+VALUES(33, 'Reino de Mariyon', 300, 'Central');
---
+++ db/db_manga/tabela_de_destino_de_exportacao.sql
@@ -0,0 +1,8 @@
+CREATE TABLE destinos_exportacao(
+    codigo_destino_de_exportacao INTEGER PRIMARY KEY,
+    nome_de_destino_de_exportacao TEXT,
+    populacao INTEGER,
+    distritos TEXT
+);	
+
+
