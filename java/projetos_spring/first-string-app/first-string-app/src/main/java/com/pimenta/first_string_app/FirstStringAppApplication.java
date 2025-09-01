aed1da41e1818323e28b72f1e3d33b5081648ec5
Paulo Henrique <paulopimenta6@gmail.com>
2025-09-01 00:49:12 -0300

    meus estudos de spring, kafka e ms

---
+++ java/projetos_spring/first-string-app/first-string-app/src/main/java/com/pimenta/first_string_app/FirstStringAppApplication.java
@@ -0,0 +1,13 @@
+package com.pimenta.first_string_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStringAppApplication.class, args);
+	}
+
+}
---
+++ java/projetos_spring/first-string-app/first-string-app/src/test/java/com/pimenta/first_string_app/FirstStringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimenta.first_string_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
