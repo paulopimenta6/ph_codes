dd1e6be2a315c920bd23a73fe31663bafdb23a7d
Paulo Henrique <paulopimenta6@gmail.com>
2025-09-27 06:38:42 -0300

    inclusao das variaveis das coortes para lmm

--- R/elsa/src/linear_mixed_model/lmm_novos_dados.R
+++ R/elsa/src/linear_mixed_model/lmm_novos_dados.R
@@ -15,12 +15,27 @@
 #A_K_TOT -> exceção de potássio urinário
 #A_CONSDIAFRUTAS: Consumo diário de frutas
 ################################################################################
+################################################################################
 # Carregar o pacote
 library(readxl)
-
 # Ler o arquivo
 dados_elsa_novos1 <- read_excel("./dados_elsa/novos_dados_elsa/dados_novos_elsa_parte1.xlsx")
 dados_elsa_novos2 <- read_excel("./dados_elsa/novos_dados_elsa/dados_novos_elsa_parte2.xlsx")
 # Visualizar os dados
 head(dados_elsa_novos1)
 head(dados_elsa_novos2)
+################################################################################
+################################################################################
+soma_dias <- dados_elsa_novos1$A_SOMADIAS
+tempo_atividade_fisica_moderada_por_semana <- dados_elsa_novos1$A_MINMODERADO
+tempo_caminhada <- dados_elsa_novos1$A_MINFRACO
+tempo_atividade_fisica_forte <- dados_elsa_novos1$A_MINFORTE
+soma_ponderada_tempo_atividade_fisica <- dados_elsa_novos1$A_MET
+consumo_diario_fast_food <- dados_elsa_novos1$A_CONSDIAFASTFOOD 
+consumo_diario_verduras_legumes <- dados_elsa_novos1$A_CONSDIAVERDURAS 
+consumo_diario_frutas <- dados_elsa_novos1$A_CONSDIAFRUTAS
+################################################################################
+################################################################################
+#Terceira onda da taxa de filtração glomerular
+excecao_potassio_urinario <- dados_elsa_novos1$A_K_TOT  
+consumo_diario_frutas <- dados_elsa_novos1$A_CONSDIAFRUTAS
