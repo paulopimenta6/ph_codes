8c3274d76b00987338cc53b56bb4fc834f536538
paulopimenta6 <paulopimenta6@gmail.com>
2025-11-17 22:55:31 -0300

    meus estudos de ggplot2

--- R/curso_R/r_for_data_science/script_r_for_data_science01.R
+++ R/curso_R/r_for_data_science/script_r_for_data_science01.R
@@ -91,3 +91,51 @@ ggplot2::ggplot(data = mpg, mapping = aes(displ, y = hwy)) +
   geom_point(mapping = aes(colour = class)) +
   geom_smooth()
 ################################################################################
+ggplot2::ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(colour = class)) +
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"),
+    mapping = aes(colour = class),
+    se = FALSE
+  ) +
+  geom_smooth(
+    data = filter(mpg, class == "compact"),
+    mapping = aes(colour = class),
+    se = FALSE
+  )
+################################################################################
+ggplot2::ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
+
+ggplot2::ggplot(data = diamonds) +
+  stat_count(mapping = aes(x = cut))
+
+demo <- tribble(
+  ~a, ~b,
+  "ba1", 20,
+  "bar2", 30,
+  "bar3", 40
+)
+
+ggplot2::ggplot(data = demo) +
+  geom_bar(mapping = aes(x = a, y = b), stat = "identity")
+
+ggplot2::ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = ..prop.., group = 1))
+
+ggplot2::ggplot(data = diamonds) +
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.min = min,
+    fun.max = max,
+    fun = median
+    )
+################################################################################
+ggplot2::ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, colour = cut))
+
+ggplot2::ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, colour = cut, fill = cut))
+
+ggplot2::ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity))
