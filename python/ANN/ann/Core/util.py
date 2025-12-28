9b33edf0cba07403b7595429f28b4ecc87f4a601
paulopimenta6 <paulopimenta6@gmail.com>
2025-12-28 05:01:18 -0300

    Projeto de redes neurais baseado no livro de David kopec

---
+++ python/ANN/ann/Core/util.py
@@ -0,0 +1,22 @@
+from typing import List
+from matth import exp
+
+# Funcao para calcular o produto escalar entre dois vetores
+# Definicao: https://en.wikipedia.org/wiki/Dot_product
+def dot_product(xs: List[float], ys: List[float]) -> float:
+    """Faz o produto escalar entre dois vetores."""
+    return sum(x*y for x, y in zip(xs, ys))
+
+# Sera usada uma funcao de ativacao da rede neural. 
+# Sera usada a uma funcao sigmoide, cujo objetivo e mapear qualquer valor real para o intervalo entre 0 e 1.
+# Mais detalhes podem ser vistos em: 
+# [1] https://pt.wikipedia.org/wiki/Fun%C3%A7%C3%A3o_sigmoide
+# [2] https://en.wikipedia.org/wiki/Logistic_regression 
+def sigmoid(x: float) -> float:
+    return 1.0/(1.0 + exp(-x))
+
+# derivada da função sigmoid
+def derivative_sigmoid(x: float) -> float:
+    sig: float = sigmoid(x)
+    return sig*(1-sig)     
+
--- python/ANN/ann/utils/util.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from matth import exp
-
-
-def dot_product(xs: List[float], ys: List[float]) -> float:
-    """Faz o produto escalar entre dois vetores."""
-    return sum(x*y for x, y in zip(xs, ys))
