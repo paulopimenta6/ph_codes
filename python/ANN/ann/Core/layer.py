4c56aaeb68b16450dd73fdacfeb19fb082d41ddb
paulopimenta6 <paulopimenta6@gmail.com>
2025-12-29 13:38:20 -0300

    classe layer (uma das camadas ocultas do neuronio)

--- python/ANN/ann/Core/layer.py
+++ python/ANN/ann/Core/layer.py
@@ -1,1 +1,11 @@
 from __future__ import annotations
+from typing import List, Callable, Optional
+from random import random
+from neuron import Neuron
+from util import dot_product
+
+class Layer:
+    def __init__(self, previous_layer: Optional[Layer], num_neurons: int,
+    learning_rate: float, activation_function: Callable[[float], float],
+    derivative_activation_function: Callable[[float], float]) -> None:
+    
