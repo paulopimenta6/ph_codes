b39753cab3600a0abd2bc2f24d51803e6c73634f
paulopimenta6 <paulopimenta6@gmail.com>
2026-01-16 14:02:00 -0300

    Ajuste na funcao de normalizacao para quando o valor da subtracao do maximo e minimo for 0 para evitar divisao por zero.

--- python/ANN/ann/Core/util.py
+++ python/ANN/ann/Core/util.py
@@ -4,7 +4,7 @@ from math import exp
 # Funcao para calcular o produto escalar entre dois vetores
 # Definicao: https://en.wikipedia.org/wiki/Dot_product
 def dot_product(xs: List[float], ys: List[float]) -> float:
-    """Faz o produto escalar entre dois vetores."""
+    # Faz o produto escalar entre dois vetores.
     return sum(x*y for x, y in zip(xs, ys))
 
 # Sera usada uma funcao de ativacao da rede neural. 
@@ -24,11 +24,14 @@ def derivative_sigmoid(x: float) -> float:
 # e esta funcao e a feature scaling de cada coluna para que esteja no intervalo de 0 a 1
 # ou seja, a normalizacao.
 # Todo neuronio em nossa rede gera valores entre 0 a 1 como resultado da funcao de ativacao signoide. 
-# Parece logivo que uma escala entre 0 a 1 faria sentido para os atributos do conjunto de dados de entrada tambem.
+# Parece logico que uma escala entre 0 a 1 faria sentido para os atributos do conjunto de dados de entrada tambem.
 def normalize_by_feature_scaling(dataset: List[List[float]]) -> None:
     for col_num in range(len(dataset[0])):
         column: List[float] = [row[col_num] for row in dataset]
         maximum: float = max(column)
         minimum: float = min(column)
         for row_num in range(len(dataset)):
-            dataset[row_num][col_num]= (dataset[row_num][col_num]-minimum)/(maximum-minimum)  
+            if maximum - minimum == 0:
+                dataset[row_num][col_num]=0.0
+            else:
+                dataset[row_num][col_num]=(dataset[row_num][col_num]-minimum)/(maximum-minimum)  
--- python/ANN/ann/examples/iris_test.py
+++ python/ANN/ann/examples/iris_test.py
@@ -12,3 +12,17 @@ if __name__=="__main__":
 
     # Carrega o conjunto de dados Iris do arquivo CSV
     with open('data/iris.csv', mode = 'r') as iris_file:
+        irises: List = list(csv.reader(iris_file)) # Le todas as linhas do arquivo CSV
+        shuffle(irises) # Embaralha as linhas para garantir aleatoriedade
+        for iris in irises:
+            parameters: List[float] = [float(n) for n in iris[0:4]]
+            iris_parameter.append(parameters) # Adiciona os parametros da flor
+            species: str = iris[4]
+            if species == "Iris-setosa":
+                iris_classifications.append([1.0,0.0,0.0]) # One-hot encoding para Iris-setosa
+            elif species == "Iris-versicolor":
+                iris_classifications.append([0.0,1.0,0.0]) # One-hot encoding para Iris-versicolor
+            else:
+                iris_classifications.append([0.0,0.0,1.0]) # One-hot encoding para Iris-virginica
+            iris_species.append(spencies) # Armazena o nome da especie
+    normalize_by_feature_scaling(iris_parameter) # Normaliza os parametros usando feature scaling
