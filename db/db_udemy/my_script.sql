7a67a4c057d2cb12b488d52e21a673cb49f7d60e
paulopimenta6 <paulopimenta6@gmail.com>
2026-01-31 04:33:04 -0300

    meus estudos de bancos de dados do udemy

--- db/db_udemy/my_script.sql
+++ db/db_udemy/my_script.sql
@@ -30,3 +30,90 @@ INSERT INTO cliente VALUES('ANA','F','ANA@GLOBO.COM',85548962,'548556985','PRES 
 INSERT INTO cliente(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('CLARA','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','883665843',999999999);
 INSERT INTO cliente(NOME,SEXO,ENDERECO,TELEFONE,CPF) 
 VALUES('CLARA','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','883665843',222222222);
+
+/*Criando uma nova tabela usando foreign key*/
+create database comercio;
+use comercio;
+-- criando tabelas
+-- tabela cliente
+create table cliente(
+idcliente int primary key auto_increment,
+nome varchar(30) not null,
+sexo enum("M", "F") not null,
+email varchar(50) unique,
+cpf varchar(15) unique
+);
+-- tabela endereco
+create table endereco(
+idendereco int primary key auto_increment,
+rua varchar(30) not null,
+bairro varchar(30) not null,
+cidade varchar(30) not null,
+estado char(2) not null,
+id_cliente int unique,
+
+foreign key(id_cliente)
+references cliente(idcliente)
+);
+-- tabela telefone
+create table telefone(
+idtelefone int primary key auto_increment,
+tipo enum("res", "com", "cel") not null,
+numero varchar(10) not null,
+id_cliente int,
+
+foreign key(id_cliente)
+references cliente(idcliente)
+);
+
+/* ENDERECO - OBRIGATORIO 
+CADASTRO DE SOMENTE UM.
+
+TELEFONE - NAO OBRIGATORIO
+CADASTRO DE MAIS DE UM (OPCIONAL) */
+
+/* CHAVE ESTRANGEIRA É A CHAVE PRIMARIA DE UMA TABELA
+QUE VAI ATÉ A OUTRA TABELA PARA FAZER REFERENCIA ENTRE
+REGISTROS */
+
+-- inserindo dados
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Joao", "M", "joaoa@ig.com", "76567587887"); 
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Carlos", "M", "carlosa@ig.com", "5464553466");
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Ana", "F", "ana@ig.com", "456545678");
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Clara", "F", NULL, "5687766856");
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Jorge", "M", "jorge@ig.com", "8756547688");
+insert into cliente(idcliente, nome, sexo, email, cpf) values(NULL, "Celia", "M", "jcelia@ig.com", "5767876889"); 
+
+-- verificando os dados inseridos
+select * from cliente;
+-- select usando count para verificar os grupos diferentes
+select count(*) as quantidade, sexo 
+from cliente
+group by sexo;
+-- descrevendo a tabela endereco
+desc endereco;
+-- inserindo dados na tabela endereco
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua Antonio Sa", "Centro", "Belo Horizonte", "MG", 4);
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua Capitao Hermes", "Centro", "Rio de Janeiro", "RJ", 1);
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua Pres. Vargas", "Jardins", "Sao Paulo", "SP", 3);
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua da Alfandega", "Estacio", "Rio de Janeiro", "RJ", 2);
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua do Ouvidor", "Flamengo", "Rio de Janeiro", "RJ", 6);
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua da Uruguaiana", "Centro", "Vitoria", "ES", 5);
+-- aqui nao dara certo, pois o id_cliente na tabela endereco e unique, logo ele associa um id_cliente a um idcliente de cliente
+insert into endereco(idendereco, rua, bairro, cidade, estado, id_cliente) values(NULL, "Rua da Alfandega", "Centro", "Sao Paulo", "SP", 5);
+-- verificando os dados inseridos
+select * from endereco;
+
+-- inserindo os dados na tabela telefone
+insert into telefone(idtelefone, tipo, numero, id_cliente) values(NULL, "cel", "78458743", 5);
+insert into telefone(idtelefone, tipo, numero, id_cliente) values(NULL, "res", "56576876", 5);
+insert into telefone(idtelefone, tipo, numero, id_cliente) values(NULL, "cel", "87866896", 1);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "com", "54768899", 2);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "res", "99667587", 1);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "cel", "78989765", 3);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "cel", "99766676", 3);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "com", "66687899", 1);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "res", "89986668", 5);
+INSERT INTO telefone(idtelefone, tipo, numero, id_cliente) VALUES(NULL, "cel", "88687909", 2);
+
+
